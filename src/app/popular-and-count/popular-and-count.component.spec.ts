b06e3266e523a701fb6c43b32f64862985d22b90
sgc-fk8 <sgc_ap2@icloud.com>
2024-06-13 16:14:23 -0400

    Create popular-and-count.component.spec.ts

---
+++ src/app/popular-and-count/popular-and-count.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PopularAndCountComponent } from './popular-line-with-count.component';
+
+describe('PopularAndCountComponent', () => {
+  let component: PopularAndCountComponent;
+  let fixture: ComponentFixture<PopularAndCountComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PopularAndCountComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PopularAndCountComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
