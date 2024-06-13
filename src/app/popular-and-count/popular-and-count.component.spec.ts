9b9157f1b5a4a323171dd5e183ad887f7ff2e590
sgc-fk8 <sgc_ap2@icloud.com>
2024-06-13 17:05:22 -0400

    Update popular-and-count.component.spec.ts

--- src/app/popular-and-count/popular-and-count.component.spec.ts
+++ src/app/popular-and-count/popular-and-count.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PopularAndCountComponent } from './popular-line-with-count.component';
+import { PopularAndCountComponent } from './popular-and-count.component';
 
 describe('PopularAndCountComponent', () => {
   let component: PopularAndCountComponent;
